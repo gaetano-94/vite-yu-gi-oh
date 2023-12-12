7c3ceaf3c98297b6089de9c4508599f13c75db99
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-12 21:23:11 +0100

    feat: found

---
+++ src/components/AppMainFound.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'MainFound',
+  props: ['found'],
+};
+</script>
+
+<template>
+  <h3>Found {{ found }} cards</h3>
+</template>
+
+<style scoped lang="scss"></style>
