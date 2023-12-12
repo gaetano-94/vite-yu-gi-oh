f904e5ecc52828a90b4ccbfbec5d8930457ad193
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-12 17:28:00 +0100

    feat: search

--- src/components/AppMainSearch.vue
+++ src/components/AppMainSearch.vue
@@ -4,6 +4,26 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <select name="Alien" id="">
+      <option value="1">Alien</option>
+    </select>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  width: 1140px;
+  margin: 0 auto;
+
+  select {
+    font-size: 18px;
+    margin-top: 20px;
+    margin-left: 30px;
+    width: 200px;
+    padding: 5px;
+    border-radius: 5px;
+  }
+}
+</style>
